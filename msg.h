a15e7eb9e0091ec9327d7bff87f6f02fb58a6d8a
Max Langensiepen <mlangens@emich.edu>
2013-06-05 01:07:15 -0400

    first commit

---
+++ msg.h
@@ -0,0 +1,31 @@
+#ifndef _MSG_H
+#define _MSG_H
+
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/socket.h> /* for socket(), bind(), and connect() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_ntoa() */
+#include <stdlib.h>     /* for atoi() and exit() */
+#include <string.h>     /* for memset() */
+#include <unistd.h>     /* for close() */
+
+void DieWithError(char *errorMessage); /* Error handling function */
+void HandleTCPClient(int clntSocket); /* TCP client handling function */
+int CreateTCPServerSocket(unsigned short port); /* Create TCP server socket */
+int AcceptTCPConnection(int servSock); /* Accept TCP connection request */
+
+typedef struct {
+	enum {
+		Send, Retrieve
+	} request_Type; /* same size as an unsigned int */
+	unsigned int SenderId; /* unique client identifier */
+	unsigned int RecipientId; /* unique client identifier */
+	char message[100];
+} ClientMessage; /* an unsigned int is 32 bits = 4 bytes */
+
+typedef struct {
+	enum {New, Old, No_Message} messageType; /* same size as an unsigned int */
+		unsigned int SenderId; /* unique client identifier */
+		unsigned int RecipientId; /* unique client identifier */
+		char message[100];
+	} ServerMessage; /* an unsigned int is 32 bits = 4 bytes */
+#endif //_MSG_H
